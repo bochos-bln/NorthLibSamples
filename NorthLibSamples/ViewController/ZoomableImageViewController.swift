d0615c004c19cceb562c4cd10f29bfa5a2f71b03
Ringo Müller (iMac05) <ringo.mueller@xailabs.de>
2020-05-27 11:16:28 +0200

    Added DemoVC and NorthLib framework relative to Project Folder

---
+++ NorthLibSamples/ViewController/ZoomableImageViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ZoomableImageViewController.swift
+//  NorthLibSamples
+//
+//  Created by Ringo Müller on 27.05.20.
+//  Copyright © 2020 Ringo Müller. All rights reserved.
+//
+
+import UIKit
+import NorthLib
+
+
+class ZoomableImageViewController: UIViewController {
+  
+  var detailImage: UIImage?
+//  var oi: OptionalImageItem?
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.view.backgroundColor = UIColor.red
+  }
+}
