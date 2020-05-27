85be49fa47ad1abba9c85dfae70d104272bd2fc3
Ringo Müller (iMac05) <ringo.mueller@xailabs.de>
2020-05-27 09:44:12 +0200

    Added xCode Swift Project

---
+++ NorthLibSamples/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NorthLibSamples
+//
+//  Created by Ringo Müller on 27.05.20.
+//  Copyright © 2020 Ringo Müller. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NorthLibSamplesTests/NorthLibSamplesTests.swift
@@ -0,0 +1,34 @@
+//
+//  NorthLibSamplesTests.swift
+//  NorthLibSamplesTests
+//
+//  Created by Ringo Müller on 27.05.20.
+//  Copyright © 2020 Ringo Müller. All rights reserved.
+//
+
+import XCTest
+@testable import NorthLibSamples
+
+class NorthLibSamplesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
