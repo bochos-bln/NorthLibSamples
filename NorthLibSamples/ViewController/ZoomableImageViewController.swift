1971290bfaf010a7b2b3ced7766c22634397d8fd
Ringo Müller <RINGO.mueller@xailabs.de>
2020-05-28 23:04:27 +0200

    added / extended test scenarios

--- NorthLibSamples/ViewController/ZoomableImageViewController.swift
+++ NorthLibSamples/ViewController/ZoomableImageViewController.swift
@@ -19,15 +19,16 @@ class ZoomableImageViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    if let detailImageFilePath = Bundle.main.path(forResource: "IMG_L",
+    if let detailImageFilePath = Bundle.main.path(forResource: "IMG_XL",
                                                   ofType: "jpg") {
       detailImage = UIImage(contentsOfFile: detailImageFilePath)
     }
     
-    if let waitingImageFilePath = Bundle.main.path(forResource: "IMG_S",
+    if let waitingImageFilePath = Bundle.main.path(forResource: "IMG_L",
                                                    ofType: "jpg"),
       let waitingImage = UIImage(contentsOfFile: waitingImageFilePath) {
       let _optionalImage = OptionalImageItem(waitingImage: waitingImage)
+      _optionalImage.image = self.detailImage //test if detailImage already available
       self.optionalImage = _optionalImage
       let zView = ZoomedImageView(optionalImage: _optionalImage)
       zView.onX {
@@ -40,10 +41,21 @@ class ZoomableImageViewController: UIViewController {
   override func viewDidAppear(_ animated: Bool) {
     super.viewDidAppear(animated)
     //Set Detail Image after Delay to Simulate Download
-    DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+    DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
       if let optionalImage = self.optionalImage {
+        print("Exchanged!")
         optionalImage.image = self.detailImage
       }
     }
+    /* Test #2 2nd Exchange, not required but seems to work
+     DispatchQueue.main.asyncAfter(deadline: .now() + 12.5) {
+     if let detailImageFilePath = Bundle.main.path(forResource: "IMG_XL",
+     ofType: "jpg"),
+     let optionalImage = self.optionalImage {
+     print("Exchanged #2!")
+     optionalImage.image = UIImage(contentsOfFile: detailImageFilePath)
+     }
+     }
+     */
   }
 }
